f927f038473e7e16bdf2473a44c3eaa1d875dfb6
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-05-21 00:25:44 +0200

    Change let on const

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let score=0;
-let scoreElement=document.getElementById('score')
+{
+const score=0;
+const scoreElement=document.getElementById('score')
 console.log (scoreElement)
 
 function playGame(playerInput){
@@ -68,11 +69,11 @@ function playGame(playerInput){
     }
   }
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
 
 /*if(randomNumber == '1'){
   computerMove = 'kamień';
@@ -87,7 +88,7 @@ else if (randomNumber == '3') {
 
   console.log('Gracz wpisał: ' + playerInput);
 
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
 
 /*if(playerInput == '1'){
   playerMove = 'kamień';
@@ -112,3 +113,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame('3');
 });
+}
